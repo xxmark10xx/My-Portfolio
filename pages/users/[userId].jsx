e806e338e0cdbccb84ef90348a32a067031684b2
xxmark10xx <mark.a.aguilar99@gmail.com>
2022-04-08 16:58:00 -0700

    fixing the errors with user file

--- pages/users/[userId].jsx
+++ pages/users/[userId].jsx
@@ -2,7 +2,7 @@
 import { useRouter } from "next/router";
 
 
-export default function userIdPage() {
+export default function UserIdPage() {
     // open an instance of useRouter
     const router = useRouter()
     // pull the path param (called query) from the router object
